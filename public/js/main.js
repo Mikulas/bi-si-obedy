27da9c2178c4daf0033cf76f7b531bd94ad4c21c
Mikuláš Dítě <ditemiku@fit.cvut.cz>
2016-04-27 23:08:25 +0200

    implement flash message close button

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
-$(function(){
-
+$(function() {
+    $(".notification .delete").on('click', function() {
+        var $notification = $(this).parent('.notification');
+        $notification.slideUp();
+    });
 });
